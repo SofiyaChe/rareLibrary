33dacd9d4b01bba7d694e1553140bc8d01bbf78e
Admin <sofiya.chebotareva@bk.ru>
2025-08-04 19:12:32 +0200

    change branch feature-a

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 33 дня");
+        System.out.println("Открытие магазина ожидается через 4 дня");
     }
 }
